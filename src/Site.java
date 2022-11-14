44f407ad7a91cecfa9ae89d10e2b4d53e12873e1
Shahrik Amin <shahrikamin2001@gmail.com>
2022-11-14 15:51:03 -0500

    Refactored getBillableAmount and getTaxAmount, getBaseAmount in site.java for the file method example.

--- src/Site.java
+++ src/Site.java
@@ -1,3 +1,16 @@
 public class Site {
-    public static double TAX_RATE = 1.13;
+    public static final double TAX_RATE = 1.13;
+
+    public double getBillableAmount(double _units, double _rate) {
+        return getBaseAmount(_units, _rate) + getTaxAmount(getBaseAmount(_units, _rate));
+    }
+
+    private double getTaxAmount(double base) {
+        double tax = base * Site.TAX_RATE;
+        return tax;
+    }
+
+    private double getBaseAmount(double units, double rate) {
+        return units * rate;
+    }
 }
