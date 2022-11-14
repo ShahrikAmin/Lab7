37c13e0e2f21ba8adbb204cce544078aa2753f97
Shahrik Amin <shahrikamin2001@gmail.com>
2022-11-14 15:49:16 -0500

    Removed getBillableAmount in ResidentialSite

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,18 +1,14 @@
 public class ResidentialSite {
 
 
-    public double getBillableAmount(double _units, double _rate) {
-        double base = getBaseAmount(_units, _rate);
-        double tax = getTax(base);
-        return base + tax;
-    }
 
-    private double getTax(double base) {
+
+    public double getTaxAmount(double base) {
         double tax = base * Site.TAX_RATE;
         return tax;
     }
 
-    private double getBaseAmount(double _units, double _rate) {
+    public double getBaseAmount(double _units, double _rate) {
         return _units * _rate;
     }
 }
