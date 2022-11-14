46f5c70a4495777eb37bd196741096ec0e72597a
Shahrik Amin <shahrikamin2001@gmail.com>
2022-11-14 15:50:06 -0500

    Refactored tax in LifelineSite

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -2,8 +2,13 @@ public class LifelineSite {
 
     public double getBillableAmount(double _units, double _rate) {
         double base = getBaseAmount(_units, _rate);
-        double tax = base * Site.TAX_RATE * 0.2;
+        double tax = getTaxAmount(base);
         return base + tax;
+    }
+
+    private double getTaxAmount(double base) {
+        double tax = base * Site.TAX_RATE * 0.2;
+        return tax;
     }
 
     private double getBaseAmount(double _units, double _rate) {
